917c1fa9f9e25510f3a62f4eb94b2e1f1dfa10e5
pandaman64 <kointosudesuyo@infoseek.jp>
2016-02-05 20:32:53 +0900

    Read PE file header (not working)

--- source/app.d
+++ source/app.d
@@ -2,6 +2,18 @@ import std.stdio;
 import std.range;
 import std.algorithm;
 import std.system;
+import std.datetime;
+
+align(1)
+struct PE_file_header{
+	ushort machine;
+	ushort number_of_sections;
+	uint timestamp;
+	uint pointer_to_symbol_table;
+	uint number_of_symbols;
+	ushort optional_header_size;
+	ushort characteristics;
+}
 
 auto read(T,R)(ref R range,Endian endian = Endian.littleEndian)
 if(isInputRange!R && is(ElementType!R == ubyte)){
@@ -46,6 +58,26 @@ struct Assembly{
 		//MS-DOS header shall have exactly 128 bytes so PE signature must be placed 128 byte after the beginning
 		assert(lfanew >= 128);
 		range.popFrontN(lfanew - 4 - 0x3c);
+
+		//Read PE file header in PE header
+		PE_file_header pe_file_header = void;
+		pe_file_header.machine = range.read!ushort;
+		pe_file_header.number_of_sections = range.read!ushort;
+		pe_file_header.timestamp = range.read!uint;
+		pe_file_header.pointer_to_symbol_table = range.read!uint;
+		pe_file_header.number_of_symbols = range.read!uint;
+		pe_file_header.optional_header_size = range.read!ushort;
+		pe_file_header.characteristics = range.read!ushort;
+		pe_file_header.sizeof.writeln;
+		pe_file_header.writeln;
+		assert(pe_file_header.machine == 0x14c);
+		assert(pe_file_header.pointer_to_symbol_table == 0);
+		assert(pe_file_header.number_of_symbols == 0);
+		writefln("sections : %d\ntime : %s\nheader size : %d\ncharacteristics : %d",
+				pe_file_header.number_of_sections,
+				pe_file_header.timestamp.unixTimeToStdTime,
+				pe_file_header.optional_header_size,
+				pe_file_header.characteristics);
 	}
 }
 
